2cb55750a43e0c64b4498f7c080c123b575e3003
s26834-pj <115928176+s26834-pj@users.noreply.github.com>
2022-12-29 20:32:38 +0100

    Add files via upload

---
+++ Zadanie_5.2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tablica[7][7], a, b;
+	int i, j, k;
+	
+	cout << "Podaj liczbe wierszy (nie wieksza niz 7): ";
+	cin >> a;
+	cout << "Podaj liczbe kolumn (nie wieksza niz 7): ";
+	cin >> b;
+	
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			cout << "Podaj element " << i + 1 << " wiersza i " << j + 1 << " kolumny tablicy: ";
+			cin>> tablica[i][j];
+		}
+	}
+	
+	cout << endl << "Tablica wejsciowa:" << endl;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			cout << tablica[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
+	for (i = 0; i < a; i++)
+	{
+		for (j = 0; j < b / 2; j++)
+		{
+			k = tablica[i][j];
+			tablica[i][j] = tablica[i][(b - 1) - j];
+			tablica[i][(b - 1) - j] = k;
+		}
+	}
+	
+	cout << endl << "Tablica wyjsciowa:" << endl;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			cout << tablica[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
