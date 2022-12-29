0c39b1fec9e7bbfafd11b791cdc4d8ccf985cc81
s26834-pj <115928176+s26834-pj@users.noreply.github.com>
2022-12-29 19:41:21 +0100

    Add files via upload

---
+++ Zadanie_5.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    int max = 0;
+   
+    cout << "Podaj rozmiar vektora ";
+    cin >> n;
+
+    vector <int> ns(n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Podaj " << i + 1 << " element vektora ";
+        cin >> ns[i];
+
+
+        if (ns[i] > ns[max])
+        {
+            max = i;
+        }
+    }
+    cout << endl << "Wartosc elementu maksymalnego wektora wynosi: " << ns[max] << endl;
+    cout << "Numer indeksu elementu maksymalnego vektora: " << max<< endl << endl;
+    
+    return 0;
+}
